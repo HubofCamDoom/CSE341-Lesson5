81c0deeb76ca80167f4d1e020121d455877f5a4d
Camden Asay <chasay21@gmail.com>
2025-06-07 21:30:32 -0800

    auth0 attempt

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
+const nJwt = require('njwt');
+const secretKey = process.env.SECRET;
 const port = process.env.PORT || 8080;
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+
+    // Authorization middleware. When used, the Access Token must
+    // exist and be verified against the Auth0 JSON Web Key Set.
+    const checkJwt = auth({
+      tokenSigningAlg: "HS256",
+      audience: process.env.CLIENT_ID,
+      issuerBaseURL: process.env.ISSUER_BASE_URL
+    });
+
+    // This route doesn't need authentication
+    app.get('/api/public', function(req, res) {
+      res.json({
+message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+      });
+    });
+
+    // This route needs authentication
+    app.get('/api/private', checkJwt, function(req, res) {
+      res.json({
+message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+      });
+    });
+
+    const checkScopes = requiredScopes('read:messages');
+
+    app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
+      res.json({
+message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+      });
+    });
 
 app
   .use(bodyParser.json())
